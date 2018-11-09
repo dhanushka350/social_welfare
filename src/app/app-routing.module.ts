5b26808b524bf6dab6f66cdd467b5bbf17c2d15b
Chandimal <Schandimal>
2018-11-09 18:24:44 +0530

    ui complete

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,15 @@ import {GramaNiladhariDivisionComponent} from './grama-niladhari-division/grama-
 import {MonthlyPaymentReportComponent} from './monthly-payment-report/monthly-payment-report.component';
 import {AdharaLabhinReportComponent} from './adhara-labhin-report/adhara-labhin-report.component';
 import {DistrictReportComponent} from './district-report/district-report.component';
+import {DisabledEldersComponent} from './disabled-elders/disabled-elders.component';
+import {VoluntaryOrganizationsInformationComponent} from './voluntary-organizations-information/voluntary-organizations-information.component';
+import {DivisionalSecretariatSelectComponent} from './divisional-secretariat-select/divisional-secretariat-select.component';
+import {AllocationOfProvisionsComponent} from './allocation-of-provisions/allocation-of-provisions.component';
+import {MonthlyReportComponent} from './monthly-report/monthly-report.component';
+import {MonthlyReportTwoComponent} from './monthly-report-two/monthly-report-two.component';
+import {MonthlyReportThreeComponent} from './monthly-report-three/monthly-report-three.component';
+import {MonthlyReportFourComponent} from './monthly-report-four/monthly-report-four.component';
+import {MonthlyReportFiveComponent} from './monthly-report-five/monthly-report-five.component';
 
 const routes: Routes = [
   {
@@ -26,6 +35,42 @@ const routes: Routes = [
   {
     path: 'districtReport',
     component: DistrictReportComponent
+  },
+  {
+    path: 'disabledElders',
+    component: DisabledEldersComponent
+  },
+  {
+    path: 'voluntaryOrganizations',
+    component: VoluntaryOrganizationsInformationComponent
+  },
+  {
+    path: 'divisionalSecretariat',
+    component: DivisionalSecretariatSelectComponent
+  },
+  {
+    path: 'allocationOfProvisions',
+    component: AllocationOfProvisionsComponent
+  },
+  {
+    path: 'monthlyReport',
+    component: MonthlyReportComponent
+  },
+  {
+    path: 'monthlyReportTwo',
+    component: MonthlyReportTwoComponent
+  },
+  {
+    path: 'monthlyReportThree',
+    component: MonthlyReportThreeComponent
+  },
+  {
+    path: 'monthlyReportFour',
+    component: MonthlyReportFourComponent
+  },
+  {
+    path: 'monthlyReportFive',
+    component: MonthlyReportFiveComponent
   },
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,15 @@ import { GramaNiladhariDivisionComponent } from './grama-niladhari-division/gram
 import { MonthlyPaymentReportComponent } from './monthly-payment-report/monthly-payment-report.component';
 import { AdharaLabhinReportComponent } from './adhara-labhin-report/adhara-labhin-report.component';
 import { DistrictReportComponent } from './district-report/district-report.component';
+import { DisabledEldersComponent } from './disabled-elders/disabled-elders.component';
+import { VoluntaryOrganizationsInformationComponent } from './voluntary-organizations-information/voluntary-organizations-information.component';
+import { DivisionalSecretariatSelectComponent } from './divisional-secretariat-select/divisional-secretariat-select.component';
+import { AllocationOfProvisionsComponent } from './allocation-of-provisions/allocation-of-provisions.component';
+import { MonthlyReportComponent } from './monthly-report/monthly-report.component';
+import { MonthlyReportTwoComponent } from './monthly-report-two/monthly-report-two.component';
+import { MonthlyReportThreeComponent } from './monthly-report-three/monthly-report-three.component';
+import { MonthlyReportFourComponent } from './monthly-report-four/monthly-report-four.component';
+import { MonthlyReportFiveComponent } from './monthly-report-five/monthly-report-five.component';
 
 @NgModule({
   declarations: [
@@ -20,7 +29,16 @@ import { DistrictReportComponent } from './district-report/district-report.compo
     GramaNiladhariDivisionComponent,
     MonthlyPaymentReportComponent,
     AdharaLabhinReportComponent,
-    DistrictReportComponent
+    DistrictReportComponent,
+    DisabledEldersComponent,
+    VoluntaryOrganizationsInformationComponent,
+    DivisionalSecretariatSelectComponent,
+    AllocationOfProvisionsComponent,
+    MonthlyReportComponent,
+    MonthlyReportTwoComponent,
+    MonthlyReportThreeComponent,
+    MonthlyReportFourComponent,
+    MonthlyReportFiveComponent
   ],
   imports: [
     BrowserModule,
